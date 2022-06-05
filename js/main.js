84c27b5e446dc87abf2f3e589343c4ffc9d4dbf1
jfang96 <jfang.96@gmail.com>
2022-06-04 23:09:30 -0700

    Add comment on Learn More button function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
+
+    // Learn More button scrolls to basic info
     $(".learnmore-button").click(function () {
         $('html,body').animate({
             scrollTop: $(".basic-info-container").offset().top},
             'slow');
         console.log('hi');
     });
+
 });
