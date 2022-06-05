130013e6aed471c39b16403df6b0b02dc5c461a1
jfang96 <jfang.96@gmail.com>
2022-06-04 23:20:37 -0700

    Implement scroll to top button

--- js/main.js
+++ js/main.js
@@ -3,9 +3,32 @@ $(document).ready(function () {
     // Learn More button scrolls to basic info
     $(".learnmore-button").click(function () {
         $('html,body').animate({
-            scrollTop: $(".basic-info-container").offset().top},
+            scrollTop: $(".basic-info-container").offset().top
+        },
             'slow');
         console.log('hi');
+    });
+
+    $('.js-gotop').on('click', function (event) {
+
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $('html').offset().top
+        }, 1000, 'easeInOutExpo');
+
+        return false;
+    });
+
+    $(window).scroll(function () {
+
+        var $win = $(window);
+        if ($win.scrollTop() > 200) {
+            $('.js-top').addClass('active');
+        } else {
+            $('.js-top').removeClass('active');
+        }
+
     });
 
 });
